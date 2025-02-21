0f9aaeaa3b54069424f8aebbb0a62a59906116bc
Ahmed Abdulsalam <salshehu@gmail.com>
2025-02-21 06:57:28 +0000

    complete header config

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   let options = {
-//     strings: [
-//       "Full Stack Developer",
-//       "Management Accountant",
-//       "Financial Auditor",
-//       "Tax Consultant",
-//     ],
-//     typeSpeed: 50,
-//     backSpeed: 25,
-//     loop: true,
-//   };
+/**** TypedJs functionality */
+// let options = {
+//   strings: [
+//     "Full Stack Developer",
+//     "Management Accountant",
+//     "Financial Auditor",
+//     "Tax Consultant",
+//   ],
+//   typeSpeed: 50,
+//   backSpeed: 25,
+//   loop: true,
+// };
 
-//   let typed = new Typed("#typed-output", options);
-// });
+// let typed = new Typed("#typed-output", options);
 
-let sections = document.querySelectorAll("section");
-let navlinks = document.querySelectorAll("header nav a");
-
+/**** Menu tab selection highlighting functionality */
 window.onscroll = () => {
+  const sections = document.querySelectorAll("section");
+  const navlinks = document.querySelectorAll("header nav a");
   sections.forEach((sec) => {
-    let id = sec.getAttribute("id");
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
+    const id = sec.getAttribute("id");
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
 
     if (top >= offset && top < offset + height) {
       navlinks.forEach((link) => {
         link.classList.remove("active");
 
-        let lnk = document.querySelector(".navbar a[href*=" + id + "  ]");
+        const lnk = document.querySelector(".navbar a[href*=" + id + "  ]");
         lnk.classList.add("active");
       });
     }
   });
+
+  /**** sticky navbar functionality */
+  const stickyHead = document.querySelector("header");
+  stickyHead.classList.toggle("sticky", window.scrollY > 100);
 };
